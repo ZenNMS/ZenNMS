8ca95d6ea6f175ef7ba1289cab305bd8c9166e6d
angelcamposm <angel.campos.m@outlook.com>
2021-06-19 12:49:16 +0200

    WIP: Created svg status components.

---
+++ resources/views/components/ui/atoms/circle-not-present-status.blade.php
@@ -0,0 +1,8 @@
+<svg aria-hidden="true" {{ $attributes->merge(['class' => '']) }} focusable="false" data-icon="circle" data-prefix="fad" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512">
+  <g class="fa-group">
+    <path
+        fill="#d2d5d9"
+        d="M512 256c0 141.4-114.6 256-256 256s-256-114.6-256-256s114.6-256 256-256S512 114.6 512 256z"></path>
+    <path fill="currentColor" d=""></path>
+  </g>
+</svg>
