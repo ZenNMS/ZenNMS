2358c88e86410e098f32fba3b94baa1238472b71
angelcamposm <angel.campos.m@outlook.com>
2021-06-19 13:15:20 +0200

    WIP: Updated DashboardController.php

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -10,10 +10,10 @@ class DashboardController extends Controller
      * Handle the incoming request.
      *
      * @param  \Illuminate\Http\Request  $request
-     * @return \Illuminate\Http\Response
+     * @return \Illuminate\Contracts\View\View
      */
     public function __invoke(Request $request)
     {
-        return view('pages.dashboard.details');
+        //;
     }
 }
