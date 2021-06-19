1cdf1c6692f3c6b2aa5b88910acde6462db1bb3d
angelcamposm <angel.campos.m@outlook.com>
2021-06-19 15:54:00 +0200

    WIP: Updated card component.

--- resources/views/components/ui/atoms/card.blade.php
+++ resources/views/components/ui/atoms/card.blade.php
@@ -1,5 +1,5 @@
 <!-- Begin::Card -->
-<div class="bg-white shadow overflow-hidden mb-4">
+<div {{ $attributes->merge(['class' => 'bg-white shadow overflow-hidden mb-4']) }}>
   {{ $slot }}
 </div>
 <!-- End::Card -->
