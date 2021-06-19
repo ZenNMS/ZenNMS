eff136cc9576eef237b40ec64e45aeb147921c4d
angelcamposm <angel.campos.m@outlook.com>
2021-06-19 18:28:16 +0200

    WIP: Added label component.

--- resources/views/components/ui/atoms/label.blade.php
+++ resources/views/components/ui/atoms/label.blade.php
@@ -1,5 +1,18 @@
 @props([
-  'value' => 0
+  'type' => 'success'
 ])
 
-<span {{ $attributes->merge(['class' => 'rounded-full text-xs py-0.5 px-2']) }} class="bg-green-100 text-green-600">{{ $value }}</span>
+@php
+$class = match($type) {
+    'danger'  => 'bg-red-100 text-red-600',
+    'default' => 'bg-gray-100 text-gray-600',
+    'info'    => 'bg-blue-100 text-blue-600',
+    'light'   => 'bg-gray-50 text-gray-500',
+    'success' => 'bg-green-100 text-green-600',
+    'warning' => 'bg-yellow-100 text-yellow-600',
+}
+@endphp
+
+<span {{ $attributes->merge(['class' => 'rounded-full text-xs py-0.5 px-2 '.$class]) }}>
+  {{ $slot }}
+</span>
