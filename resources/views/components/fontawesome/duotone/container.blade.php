984773a3a19d35e9f70fe6c8fc882b67253b5166
angelcamposm <angel.campos.m@outlook.com>
2021-06-19 18:09:18 +0200

    WIP: Added more fontawesome icons for the entities page.

---
+++ resources/views/components/fontawesome/duotone/container.blade.php
@@ -0,0 +1,19 @@
+<!-- Begin::SVG-->
+<svg {{ $attributes->merge(['class' => '']) }}
+     aria-hidden="true"
+     focusable="false"
+     data-prefix="fad"
+     data-icon="container-storage"
+     role="img"
+     xmlns="http://www.w3.org/2000/svg"
+     viewBox="0 0 640 512">
+  <g class="fa-group">
+    <path class="fa-secondary"
+          fill="currentColor"
+          d="M16 96v320h608V96zm96 288H80V128h32zm112 0h-32V128h32zm112 0h-32V128h32zm112 0h-32V128h32zm112 0h-32V128h32z"></path>
+    <path class="fa-primary"
+          fill="currentColor"
+          d="M624 416H16a16 16 0 0 0-16 16v32a16 16 0 0 0 16 16h608a16 16 0 0 0 16-16v-32a16 16 0 0 0-16-16zm0-384H16A16 16 0 0 0 0 48v32a16 16 0 0 0 16 16h608a16 16 0 0 0 16-16V48a16 16 0 0 0-16-16z"></path>
+  </g>
+</svg>
+<!-- End::SVG -->
