6ce86ee32347cb7680dcff3afc97e3920bdc9243
angelcamposm <angel.campos.m@outlook.com>
2021-06-23 19:58:21 +0200

    WIP: Updated card components.

--- resources/views/components/ui/atoms/card-header.blade.php
+++ resources/views/components/ui/atoms/card-header.blade.php
@@ -1,3 +1,3 @@
-<div class="px-4 py-5 sm:px-6">
+<div class="px-4 py-4 sm:px-6">
   {{ $slot }}
 </div>
--- resources/views/components/ui/atoms/card-title.blade.php
+++ resources/views/components/ui/atoms/card-title.blade.php
@@ -1,3 +1,3 @@
-<h3 {{ $attributes->merge(['class' => 'text-lg leading-6 font-medium text-gray-900']) }}>
+<h3 {{ $attributes->merge(['class' => 'font-normal text-sm text-gray-500 tracking-wider leading-6 uppercase']) }}>
   {{ $slot }}
 </h3>
