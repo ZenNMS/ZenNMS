56893bf77e0d39e67e0633ee2c6058a7813511d2
angelcamposm <angel.campos.m@outlook.com>
2021-06-25 20:00:07 +0200

    WIP: Created InterfacesController.php

---
+++ app/Http/Controllers/InterfacesController.php
@@ -0,0 +1,91 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\DeviceInterface;
+use Illuminate\Http\Request;
+
+class InterfacesController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param \App\Models\DeviceInterface $interface
+     *
+     * @return \Illuminate\Contracts\View\View
+     */
+    public function show(DeviceInterface $interface)
+    {
+        return view('zen.interface.show', [
+            'device' => $interface->device()
+                ->select(['id', 'hostname', 'display_name'])
+                ->first(),
+            'interface' => $interface,
+        ]);
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function edit($id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        //
+    }
+}
