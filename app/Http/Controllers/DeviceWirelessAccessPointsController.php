0b1c74911b9ed7889d8d70f5d89a19f3036ad5ce
angelcamposm <angel.campos.m@outlook.com>
2021-06-25 19:26:55 +0200

    WIP: Created Wireless Access Point Controller.

---
+++ app/Http/Controllers/DeviceWirelessAccessPointsController.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Device;
+use Illuminate\Http\Request;
+
+class DeviceWirelessAccessPointsController extends Controller
+{
+    /**
+     * Handle the incoming request.
+     *
+     * @param \Illuminate\Http\Request $request
+     * @param \App\Http\Controllers\Device $device
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function __invoke(Request $request, Device $device)
+    {
+        return view('zen.device.device-access-points', [
+            'device' => $device,
+            'access_points' => $device->wirelessAccessPoints()
+                ->orderBy('name', 'asc')
+                ->paginate(12),
+        ]);
+    }
+}
