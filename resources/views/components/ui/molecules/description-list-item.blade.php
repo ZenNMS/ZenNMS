de0908a9553799cc05d350c59ab3b07e1a766d34
angelcamposm <angel.campos.m@outlook.com>
2021-06-25 19:32:05 +0200

    WIP: Updated description-list-item.blade.php component.

--- resources/views/components/ui/molecules/description-list-item.blade.php
+++ resources/views/components/ui/molecules/description-list-item.blade.php
@@ -2,9 +2,11 @@
   'term' => '',
 ])
 
-<x-html.description-list-term>
-  {{ $term }}
-</x-html.description-list-term>
-<x-html.description-list-definition class="mt-1 sm:mt-0 sm:col-span-2">
-  {{ $slot }}
-</x-html.description-list-definition>
+<div class="bg-white px-4 py-1 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-6">
+  <x-html.description-list-term>
+    {{ $term }}
+  </x-html.description-list-term>
+  <x-html.description-list-definition class="mt-1 sm:mt-0 sm:col-span-2">
+    {{ $slot }}
+  </x-html.description-list-definition>
+</div>
