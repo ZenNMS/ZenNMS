136a99a5d29efcba66e62fce76db16b31f0f65d0
angelcamposm <angel.campos.m@outlook.com>
2021-06-25 19:44:12 +0200

    WIP: Updated app left-menu to display filter menu on devices route.

--- resources/views/zen/partials/left-menu.blade.php
+++ resources/views/zen/partials/left-menu.blade.php
@@ -6,5 +6,8 @@
 <div class="flex-shrink-0 overflow-y-auto w-72 bg-white border-r border-gray-200 pt-5">
 {{--  @includeWhen($is_get_method && count($segments) === 1 && $is_valid_segment, 'zen.device.left-menu')--}}
   @includeWhen(in_array('device', $request->segments()), 'zen.device.left-menu')
+
+  @includeWhen(in_array('devices', $request->segments()), 'zen.devices.left-menu')
+
 </div>
 <!-- Begin::LeftMenu -->
