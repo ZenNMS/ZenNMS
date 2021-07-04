068a6ffbe10ca5de8ae3c5349bffb33ed8171c26
angelcamposm <angel.campos.m@outlook.com>
2021-07-04 02:23:54 +0200

    WIP: Updated card-body.blade.php component

--- resources/views/components/ui/atoms/card-body.blade.php
+++ resources/views/components/ui/atoms/card-body.blade.php
@@ -1,3 +1,3 @@
-<div {{ $attributes->merge(['class' => 'border-t border-gray-200 flex py-4']) }}>
+<div {{ $attributes->merge(['class' => 'border-t border-gray-200 py-4']) }}>
   {{ $slot }}
 </div>
